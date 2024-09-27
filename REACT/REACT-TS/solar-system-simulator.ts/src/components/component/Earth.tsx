1ded9740a10e93117b912b0cbf284ffc9c9cf47f
VictorBortolotto <victorcamposbortolotto@gmail.com>
2024-09-26 23:45:27 -0300

    Add new planets styles

--- REACT/REACT-TS/solar-system-simulator.ts/src/components/component/Earth.tsx
+++ REACT/REACT-TS/solar-system-simulator.ts/src/components/component/Earth.tsx
@@ -2,6 +2,11 @@ import React from "react";
 import '../component.style/Earth.css'
 
 export default function Earth() {
-  return <div className="earth">
-  </div>
+  return (
+    <div className="earth">
+      <div className="continent continent-1"></div>
+      <div className="continent continent-2"></div>
+      <div className="continent continent-3"></div>
+    </div>
+  );
 }
